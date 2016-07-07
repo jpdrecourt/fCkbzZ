a98c140c57f01c9922b30512712b3c7413922aa7
Jean-Philippe Drecourt <jpdrecourt@gmail.com>
2016-07-07 14:22:04 +0100

    Handling remote and local configs (dirty)

--- fCkbzZ.js
+++ fCkbzZ.js
@@ -3,7 +3,17 @@ console.log('Launching twitter bot!');
 
 // Setup Twitter
 let Twit = require('twit');
-let config = require('./config.js');
+let config;
+
+// Dirty way of handling config files locally and remotely
+try {
+  // Location of config file on OpenShift
+  config = require('../data/config.js');
+} catch(err) {
+  // Local file not committed - Check config-empty.js for template
+  config = require('./config.js');
+}
+
 let T = new Twit(config);
 
 function tweeter() {
